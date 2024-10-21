bf5ebad123ae6615b65d20b2e42dd77656da9f65
Mohamed kamal soudi <mk0875734@gmail.com>
2024-10-21 16:30:32 +0300

    search view done

--- lib/Features/home/pressentation/views/widgets/best_seller_list_view.dart
+++ lib/Features/home/pressentation/views/widgets/best_seller_list_view.dart
@@ -13,7 +13,7 @@ class BestSellerListview extends StatelessWidget {
       itemBuilder: (context, index) {
         return const Padding(
           padding:  EdgeInsets.symmetric(vertical: 10),
-          child:  BestSellerListViewItem(),
+          child:  BookListViewItem(),
         );
       },
     );
--- lib/Features/searsh/presentation/views/widgets/search_views_body.dart
+++ lib/Features/searsh/presentation/views/widgets/search_views_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app_1/Core/utils/styles.dart';
+import 'package:bookly_app_1/Features/home/pressentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app_1/Features/searsh/presentation/views/widgets/custom_search_text_field.dart';
 import 'package:flutter/material.dart';
 
@@ -11,9 +13,40 @@ Widget build(BuildContext context) {
     child: Column(
         children: [
         CustomSearchTextField(),
+        SizedBox(
+            height:16 ,
+        ),
+        Text(
+            'Search Result',
+            style: Styles.textStyle18,
+        ),
+        SizedBox(
+            height:16 ,
+        ),
+        Expanded(
+            child: SearchResultListView()),
         ],
     ),
     );
 }
 }
 
+
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding:  EdgeInsets.symmetric(vertical: 10),
+          child:  BookListViewItem(),
+        );
+      },
+    );
+  }
+}

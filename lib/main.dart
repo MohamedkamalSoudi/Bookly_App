b9f6b82b458c008d3a49ff07b31563e03a2c53d0
Mohamed kamal soudi <mk0875734@gmail.com>
2024-10-23 23:34:59 +0300

    creat models api

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,19 @@ import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(const BooklyApp());
 }
+
 class BooklyApp extends StatelessWidget {
   const BooklyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-    routerConfig: AppRouter.router,
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData.dark().copyWith(scaffoldBackgroundColor:kPrimaryColor,
-    textTheme:  GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
-    ),
+      routerConfig: AppRouter.router,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+      ),
     );
   }
 }

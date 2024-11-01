2eff70aa585f1a19567402e8dc4ccf053a3bb649
Mohamed kamal soudi <mk0875734@gmail.com>
2024-11-01 23:59:20 +0200

    commit

--- lib/Features/home/pressentation/views/home_view.dart
+++ lib/Features/home/pressentation/views/home_view.dart
@@ -2,14 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'widgets/home_view_body.dart';
 
-class HomeView extends StatefulWidget {
+class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
-  @override
-  State<HomeView> createState() => _HomeViewState();
-}
-
-class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(

e0c92ce504435064eadfc60b9873e5b60a91364b
Mohamed kamal soudi <mk0875734@gmail.com>
2024-11-02 14:28:46 +0200

    Edit main of  Blocprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import 'package:bookly_app_1/Core/utils/api_service.dart';
 import 'package:bookly_app_1/Core/utils/app_router.dart';
 import 'package:bookly_app_1/Core/utils/service_locator.dart';
 import 'package:bookly_app_1/Features/home/pressentation/manger/featured_books_cubit.dart/featured_books_cubit.dart';
 import 'package:bookly_app_1/Features/home/pressentation/views/data/repos/home_repo_impl.dart';
 import 'package:bookly_app_1/constants.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -21,19 +19,19 @@ class BooklyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(
-          create: (context) => FeaturedBooksCubit(
-            HomeRepoImpl(
-              ApiService(
-                Dio(),
-              ),
-            ),
-          )..fetchFeaturedBooks(),
-        ),
+        // BlocProvider(
+        //   create: (context) => FeaturedBooksCubit(
+        //     HomeRepoImpl(
+        //       ApiService(
+        //         Dio(),
+        //       ),
+        //     ),
+        //   )..fetchFeaturedBooks(),
+        // ),
         BlocProvider(
           create: (context) => FeaturedBooksCubit(
               getIt.get<HomeRepoImpl>(),
-          ),
+          )..fetchFeaturedBooks(),
         )
       ],
       child: MaterialApp.router(
